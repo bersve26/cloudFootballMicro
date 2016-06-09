aa39f283a1a79a257389db28d99b1b6667bd5d19
bersve26 <bersve26@uw.edu>
2016-06-09 12:31:32 -0700

    url/predict/{team1}/{team2} produces winner. Does not do ties. url/predict/ spits out hindcast.

--- index.php
+++ index.php
@@ -37,37 +37,12 @@ $app = new Micro($di);
 
 // Define the routes here
 
-$app->get('/predict/{team1}/{team2}', function() use ($app){
-    $teams = Teams::find();
-//    $phql = "SELECT * FROM Teams";
-//    $teams = $app->modelsManager->executeQuery($phql);
-
-    $teamIDNames = array();
-    foreach($teams as $team){
-        $teamIDNames[$team->id] = $team->title;
-    }
+$app->get('/predict', function() use ($app){
+    $app->response->setHeader("Content-Type", "application/json");
+    $teamGoodness = getTeamGoodness();
     $games = Games::find();
-    $teamWeightByScore = array();
-    foreach ($games as $game){
-        //find 'goodness' of each team by adding total goals, then analyzing each game to see how 'valuable' it was
-        //to team 'strength'
-        $teamWeightByScore[$game->team1_id] += $game->score1;
-        $teamWeightByScore[$game->team2_id] +- $game->score2;
-    }
-    $teamGoodness = array();
-    foreach ($games as $game){ //this feels wrong
-        // Divide team weight by each other, then multilply by score and add to goodness. We want to add the smaller number.
-        // A high scoring team vs a low scoring team is less valuable than two high scoring teams.
-        if($teamWeightByScore[$game->team1_id] > $teamWeightByScore[$game->team2_id]){
-            $teamGoodness[$game->team2_id] += ($teamWeightByScore[$game->team2_id] * ($game->score2 * ($teamWeightByScore[$game->team2_id] / $teamWeightByScore[$game->team1_id])));
-        } elseif($game->score1 == $game->score2){
-            continue;
-        }
-        else{
-            $teamGoodness[$game->team1_id] += ($teamWeightByScore[$game->team1_id] * ($game->score1 * ($teamWeightByScore[$game->team1_id] / $teamWeightByScore[$game->team2_id])));
-        }
-    }
     //let's predict the season with these numbers.
+    //amusingly, the "official" dataset includes teamids without corresponding teams.
     $victor = array();
     $numCorrect = 0;
     foreach($games as $game){
@@ -91,6 +66,57 @@ $app->get('/predict/{team1}/{team2}', function() use ($app){
     }
 
     echo json_encode($victor);
+});
+
+$app->get('/predict/{team1}/{team2}', function($team1, $team2) use ($app) {
+    $app->response->setHeader("Content-Type", "application/json");
+    if($team1 == $team2){
+        echo json_encode(array("message" => "please select 2 teams"));
+        return;
+    }
+    $goodness = getTeamGoodness();
+    $teamIDNames = array();
+    $teams = Teams::find();
+    foreach($teams as $team){
+        $teamIDNames[$team->id] = $team->title;
+    }
+    $winner = $goodness[$team1] < $goodness[$team2] ? $teamIDNames[$team2] : $teamIDNames[$team1];
+    echo json_encode(array("teams" => array("team1" => $teamIDNames[$team1], "team2" => $teamIDNames[$team2]), "winner" => $winner));
+});
+
+function getTeamGoodness(){
+    $teams = Teams::find();
+//    $phql = "SELECT * FROM Teams";
+//    $teams = $app->modelsManager->executeQuery($phql);
+
+    $games = Games::find();
+    $teamWeightByScore = array();
+    foreach ($games as $game){
+        //find 'goodness' of each team by adding total goals, then analyzing each game to see how 'valuable' it was
+        //to team 'strength'
+        $teamWeightByScore[$game->team1_id] += $game->score1;
+        $teamWeightByScore[$game->team2_id] += $game->score2;
+    }
+    $teamGoodness = array();
+    foreach ($games as $game){ //this feels wrong
+        // Divide team weight by each other, then multilply by score and add to goodness. We want to add the smaller number.
+        // A high scoring team vs a low scoring team is less valuable than two high scoring teams.
+        if($teamWeightByScore[$game->team1_id] > $teamWeightByScore[$game->team2_id]){
+            $teamGoodness[$game->team2_id] += ($teamWeightByScore[$game->team2_id] * ($game->score2 * ($teamWeightByScore[$game->team2_id] / $teamWeightByScore[$game->team1_id])));
+        } elseif($game->score1 == $game->score2){
+            continue;
+        }
+        else{
+            $teamGoodness[$game->team1_id] += ($teamWeightByScore[$game->team1_id] * ($game->score1 * ($teamWeightByScore[$game->team1_id] / $teamWeightByScore[$game->team2_id])));
+        }
+    }
+    return $teamGoodness;
+}
+
+$app->notFound(function () use ($app) {
+    $app->response->setHeader("Content-Type", "application/json");
+    $app->response->setStatusCode(404, "Not Found")->sendHeaders();
+    echo(json_encode(array("message" => 'Invalid handle.')));
 });
 
 $app->handle();
